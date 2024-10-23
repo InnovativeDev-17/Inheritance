d11a3465d5621be5c46bc25aaf821cc4b1482672
ARYAN THORAT <bussinesswitharyanthorat@gmail.com>
2024-10-23 23:56:24 +0530

    Add files via upload

---
+++ cpp exp14a.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Uni { 
+    public:
+    string university ="Symbiosis:"; 
+    void discipline (){
+        cout<<"Engineering"<<endl;
+    }
+};
+
+class Dep : public Uni{ 
+    public:
+    string department ="Electronics & Communication"; 
+};
+
+int main(){
+    Dep d; 
+    d.discipline();
+    cout << d.university << " " << d.department << endl;
+    return 0; }
+
+/*
+Engineering
+Symbiosis: Electronics & Communication
+    */
